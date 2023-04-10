19b17c78c13b8b9d3d48e948cd893ad5edf12c43
Arun kumar Ram <31740570+karun88045@users.noreply.github.com>
2023-04-10 10:44:20 +0530

    Update example.rb
    Id commit

--- code_chef/example.rb
+++ code_chef/example.rb
@@ -1,17 +1,17 @@
 class Test
   def example
     puts "Enter Number How many Times:"
-	@num = gets.chomp.to_i
+    @num = gets.chomp.to_i
   end
   def user_input
-		@var = gets.chomp
+    @var = gets.chomp
   end
   def output
     t = example()
-	t.times do
-	  user_input
-	  puts @var
-	end
+    t.times do
+      user_input
+      puts @var
+    end
   end
 end
 
